cb8edd5bfe427ed3f86c2aed03414a6ac772ebbf
[dyad] <inventiia.agencia@gmail.com>
2026-07-25 19:55:28 -0300

    [dyad] wrote 1 file(s)

--- src/components/ProjectsGrid.tsx
+++ src/components/ProjectsGrid.tsx
@@ -132,7 +132,7 @@ const ProjectsGrid = () => {
                 className="w-full h-full object-cover transition-transform duration-1000 transition-filter duration-500 group-hover:scale-105"
                 style={{ 
                   filter: isNight 
-                    ? 'brightness(0.9) saturate(0.88) hue-rotate(-2deg) contrast(1.02)' 
+                    ? 'brightness(0.75) saturate(0.8) hue-rotate(-4deg) contrast(1.06)' 
                     : 'none' 
                 }}
               />
